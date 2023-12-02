b4247336a6edcaaa673a7c244bc221ab50e73793
leftii-dev <austin@austinbarnes.dev>
2023-12-02 09:47:22 -0800

    login button scrapped, moving to navbar

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const metadata = {
+  title: 'Shag n Wag Grooming',
+  description: 'Created by austinbarne.dev',
+}
+
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>
+        <main className='mx-52'>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
